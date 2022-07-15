d342e287c5823a97a9eaa0308ead18bde546b10c
Gabriel Maes <gabrielmaes@hotmail.com>
2022-07-15 22:17:49 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
 
 function count() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossible to count!'
-        //window.alert('[ERRO] Faltam dados!')
-       
+    let ini = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')	
+    let res = window.document.getElementById('res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('Error')
     } else {
-        res.innerHTML = 'Counting: '
+        res.innerHTML = 'Counting...'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        if (p <= 0) {
-            window.alert('Invalid interval number! Considering Interval 1.')
+        if (p <= 0){
+            window.alert('Error, considering interval 1')
             p = 1
         }
-        if (i < f) {
-            //contagem crescente
-            for(let c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} \u{1F449} `
-            }
-            
-        } else {
-            //contagem regressiva
-            for(let c = i; c >= f; c -= p) {
+        if (i < f) //contagem crescente
+        for (let c = i; c < f; c += p){
+            res.innerHTML += ` ${c} \u{1F449} `
+        } else  {
+            for (let c = i; c > f; c -= p){
                 res.innerHTML += ` ${c} \u{1F449}`
             }
-            res.innerHTML += `\u{1F3C1}`
-        }
-
-       
-        
+        } res.innerHTML += `\u{1F3C1}`
     }
+
 }
